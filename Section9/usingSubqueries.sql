638f23df20abc3aff9e89f073acf1b120ec8e7bd
GithubArnavSharma <77365987+GithubArnavSharma@users.noreply.github.com>
2021-12-21 18:05:43 -0800

    Update usingSubqueries.sql

--- Section9/usingSubqueries.sql
+++ Section9/usingSubqueries.sql
@@ -28,3 +28,15 @@ JOIN directors d ON mo.director_id = d.director_id
 WHERE mo.movie_id IN
 (SELECT movie_id FROM movie_revenues
 WHERE international_takings > domestic_takings);
+
+-- Correlated subqueries
+
+SELECT d1.first_name, d1.last_name, d1.date_of_birth, d1.nationality FROM directors d1
+WHERE d1.date_of_birth = 
+(SELECT MIN(date_of_birth) FROM directors d2
+WHERE d1.nationality = d2.nationality);
+
+SELECT m1.movie_name, m1.movie_lang, m1.movie_length FROM movies m1
+WHERE m1.movie_length = 
+(SELECT MAX(m2.movie_length) FROM movies m2
+WHERE m1.movie_lang = m2.movie_lang);
