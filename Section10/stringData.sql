b88a47ec4cda87a8c89d2071fbb6319e8ee0180c
GithubArnavSharma <77365987+GithubArnavSharma@users.noreply.github.com>
2021-12-22 18:01:37 -0800

    Update stringData.sql

--- Section10/stringData.sql
+++ Section10/stringData.sql
@@ -37,3 +37,43 @@ SELECT RIGHT(first_name, 2) FROM actors;
 SELECT REVERSE('reverse');
 
 SELECT movie_name, REVERSE(movie_name) FROM movies;
+
+-- Using the SUBSTRING function
+
+SELECT SUBSTRING('long string', 2, 6);
+
+SELECT first_name, SUBSTRING(first_name, 3, 4) FROM actors;
+
+-- Using the REPLACE function
+
+SELECT REPLACE('a cat plays with another cat', 'cat', 'dog');
+
+SELECT first_name, last_name, REPLACE(gender, 'M', 'Male') FROM actors;
+
+-- Using the UPDATE function with REPLACE
+
+UPDATE directors
+SET nationality = REPLACE(nationality, 'American', 'USA');
+
+UPDATE directors
+SET nationality = REPLACE(nationality, 'Brit', 'Engl')
+WHERE nationality = 'British';
+
+SELECT * FROM directors
+
+-- Using the SPLIT_PART function
+
+SELECT SPLIT_PART('first_name.last_name@gmail.com', '@', 1);
+SELECT SPLIT_PART('first_name.last_name@gmail.com', '@', 2);
+
+SELECT SPLIT_PART('first_name.last_name@gmail.com', '.', 3);
+
+SELECT movie_name, SPLIT_PART(movie_name, ' ', 1) AS first_word, 
+SPLIT_PART(movie_name, ' ', 2) AS second_word 
+FROM movies;
+
+-- Casting into different datatypes
+
+SELECT date_of_birth::TEXT FROM directors;
+
+SELECT SPLIT_PART(date_of_birth::TEXT, '-', 1) FROM directors;
